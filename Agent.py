8444fc5ea16c3e1c5c9416f9ffa3c9d35b131a83
Michaela Vystrcilova <Mvystrcilova1@seznam.cz>
2019-10-23 12:35:25 +0200

    first commit

--- Agent.py
+++ Agent.py
@@ -2,25 +2,31 @@ import neat
 from Environment import drive
 import gym
 from gym import wrappers, logger
+from sklearn.preprocessing import MinMaxScaler
 
 env = gym.make('CarRacing-v0')
 observation_space = env.reset()
-h = open('results', 'w+')
+h = open('results_with_1000_hiddennodes.txt', 'w+')
 class CarDrivers:
 
     def evaluate_genomes(self, genomes, config):
+        scaler = MinMaxScaler((-1, 1))
         for genome_id, genome in genomes:
             genome.fitness = 0
             observation = env.reset()
             observation = observation.reshape([27648, 1])
-            net = neat.nn.RecurrentNetwork.create(genome, config)
+            net = neat.nn.FeedForwardNetwork.create(genome, config)
             # net_observation, net_reward = drive(net)
-            for _ in range(500):
+            for _ in range(1000):
                 env.render()
                 # print(env.action_space.shape)
                 # x = env.observation_space
+                # if (_ % 10 == 0) and (_ != 0):
                 action = net.activate(observation)
-                # action = env.action_space.sample()
+                # action2 = env.action_space.sample()
+                print(action)
+                action[0] = (((action[0] - 0) * (1 - (-1))/(1-0)) + (-1))
+                print(action)
                 observation, reward, done, info = env.step(action)
                 observation = observation.reshape([27648, 1])
                 # print(reward, _)
@@ -40,7 +46,7 @@ class CarDrivers:
         population.add_reporter(stats)
         population.add_reporter(neat.Checkpointer(5))
 
-        winner = population.run(self.evaluate_genomes, 300)
+        winner = population.run(self.evaluate_genomes, 20)
 
         winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
         print(winner_net)
