7a6a17fb497b712129fbf6bdaed79849ee82ef49
Michaela Vystrcilova <Mvystrcilova1@seznam.cz>
2019-11-19 09:30:13 +0100

    print disabled

--- preprocessing.py
+++ preprocessing.py
@@ -136,7 +136,7 @@ def train_again(model_file):
     checkpoint = ModelCheckpoint(model_file, monitor='loss', verbose=1, save_best_only=True, mode='min')
     trainGen = generate_input(spec_directory='mnt/0/rgb_observations', batch_size=64)
     callbacklist = [checkpoint]
-    hist = model.fit_generator(trainGen, epochs=20, steps_per_epoch = 360, verbose=True, callbacklist=callbacklist)
+    hist = model.fit_generator(trainGen, epochs=20, steps_per_epoch = 360, verbose=True, callbacks=callbacklist)
 
     # encoder.save('/mnt/0/convolutional_network_encoder_rgb')
 
